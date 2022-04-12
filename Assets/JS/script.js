6db8a541d9f415efba5b6cff7a73bf68a464e5c9
Tyler Welker <twelker08@gmail.com>
2022-04-11 21:41:17 -0400

    Update script.js

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -31,7 +31,7 @@ var carousel = document.getElementById('carousel'),
         "https://github.com/grdnd/group-project"
     ],
     pics = [
-        "./Assets/Images/Project2.JPG",
+        "./Assets/Images/Project2.jpg",
         "./Assets/Images/TechBlog.jpg",
         "./Assets/Images/E-Commerce.JPG",
         "./Assets/Images/NoteTaker.JPG",
